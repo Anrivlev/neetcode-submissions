bef448747073537fd4fe1ff3dffcac04431d17ec
Andrey Ivlev <56135485+Anrivlev@users.noreply.github.com>
2026-09-22 16:12:04 +0300

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.js
@@ -0,0 +1,18 @@
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @param {number} target
+   * @return {number}
+   */
+  search(nums, target) {
+    let left = 0;
+    let right = nums.length;
+    while (left < right) {
+      const mid = Math.floor((left + right) / 2);
+      if (nums[mid] === target) return mid;
+      if (nums[mid] < target) left = mid + 1;
+      else right = mid;
+    }
+    return -1;
+  }
+}
